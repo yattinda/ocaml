c9a566d6fb9d10be95c8e63d3f1bed96c6a66dc1
yattinda <yattinda@gmail.com>
2021-02-19 20:23:46 +0900

    FizzBuzz at Ocaml

---
+++ fizzbuzz.ml
@@ -0,0 +1,10 @@
+let rec fizzbuzz n max =
+  if n == 57 then Printf.printf "57 is CERTAIBLY a prime number!! OK??\n"
+  else if n mod 15 == 0 then Printf.printf "%d is fizz buzz\n" n
+  else if n mod 3 == 0 && n <> 57 then Printf.printf "%d is fizz\n" n
+  else if n mod 5 == 0 then Printf.printf "%d is buzz\n" n
+  else Printf.printf "%d\n" n;
+
+  if n <= max then fizzbuzz (n + 1) max
+
+let () = fizzbuzz 1 56
