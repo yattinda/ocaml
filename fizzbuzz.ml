acb379dd82d14879224f63ddcd0286198659914f
yattinda <yattinda@gmail.com>
2021-02-19 20:25:53 +0900

    Fix spellmiss

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -1,5 +1,5 @@
 let rec fizzbuzz n max =
-  if n == 57 then Printf.printf "57 is CERTAIBLY a prime number!! OK??\n"
+  if n == 57 then Printf.printf "57 is CERTAINLY a prime number!! OK??\n"
   else if n mod 15 == 0 then Printf.printf "%d is fizz buzz\n" n
   else if n mod 3 == 0 && n <> 57 then Printf.printf "%d is fizz\n" n
   else if n mod 5 == 0 then Printf.printf "%d is buzz\n" n
