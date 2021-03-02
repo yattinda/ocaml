367ad4a47b5b1b81ac5b8f233095cf9fada5e1b9
yattinda <yattinda@gmail.com>
2021-03-02 19:20:26 +0900

    Add using match

--- practice/list_length.ml
+++ practice/list_length.ml
@@ -1,3 +1,8 @@
 let rec length ls =
   if ls = [] then 0
   else 1 + length (List.tl ls)
+
+
+let rec match_length = function
+  [] -> 0
+| x :: list -> 1 + length list
--- practice/list_reverse.ml
+++ practice/list_reverse.ml
@@ -3,3 +3,7 @@ let rec reverse list1 =
   else reverse (List.tl list1) @ [List.hd list1]
 
 (*演算子 @ は Lisp の関数 append と同じで、2つのリストをつないだリストを返します。*)
+
+let rec reverse = function
+  [] -> []
+| x :: list -> reverse list @ [x]
