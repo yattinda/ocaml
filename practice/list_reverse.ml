2510320dd5e46caa88b07360abcf7b59759b1f26
yattinda <yattinda@gmail.com>
2021-03-02 18:46:35 +0900

    Fix comment

--- practice/list_reverse.ml
+++ practice/list_reverse.ml
@@ -2,4 +2,4 @@ let rec reverse list1 =
   if list1 = [] then []
   else reverse (List.tl list1) @ [List.hd list1]
 
-演算子 @ は Lisp の関数 append と同じで、2つのリストをつないだリストを返します。
+(*演算子 @ は Lisp の関数 append と同じで、2つのリストをつないだリストを返します。*)
