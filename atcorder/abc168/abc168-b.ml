a9b1e7d35ae0439999ba4613e7742eda231922e8
yattinda <yattinda@gmail.com>
2021-03-08 00:16:03 +0900

    Fix indent

--- atcorder/abc168/abc168-b.ml
+++ atcorder/abc168/abc168-b.ml
@@ -1,6 +1,6 @@
 let num = read_int()
 let str = read_line()
-let str_num  =String.length str
+let str_num  = String.length str
 let ans = if str_num <= num then str
 			else (String.sub str 0 num) ^ "..."
 let () = print_endline ans
