a01959e977153713b32f07901317b3da1449cab3
yattinda <yattinda@gmail.com>
2021-03-24 23:50:39 +0900

    Add comment

--- practice/fib_tail.ml
+++ practice/fib_tail.ml
@@ -1,5 +1,19 @@
-let rec fib_tail n a b =
-  if n <= 0 then a else
-  fib_tail (n - 1) b (a + b)
+let fibonacci n =
+  let rec fibo (n, a1, a2) =
+    if n = 0 then a1 else fibo (n - 1, a1 + a2, a1)
+  in
+    fibo (n, 1, 0)
 
-(*nがカウンターみたいな役割*)
+(*
+
+nがカウンターみたいな役割
+
+fibo (5, 1, 0)
+  fibo (4, 1, 1)
+    fibo (3, 2, 1)
+      fibo (2, 3, 2)
+        fibo (1, 5, 3)
+          fibo (0, 8, 5)
+          => a1 の値 8 を返す
+
+*)
