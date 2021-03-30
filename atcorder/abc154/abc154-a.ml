21d339c85945aa62c3970553752738f6a1ee4dc0
yattinda <yattinda@gmail.com>
2021-03-30 18:07:58 +0900

    EZPZ

--- atcorder/abc154/abc154-a.ml
+++ atcorder/abc154/abc154-a.ml
@@ -1,12 +1,12 @@
 let tmp = read_line()
-let tmp2 = Str.split (Str.regexp " ") tmp
 let a, b = Scanf.sscanf (read_line ()) "%d %d" (fun a b -> (a, b))
 let u = read_line()
 
+let tmp2 = Str.split (Str.regexp " ") tmp
 let s = List.nth tmp2 0;;
 let t = List.nth tmp2 1;;
 
-if u == s then 
+if s = u then
 	print_endline ((string_of_int (a - 1)) ^ " " ^ (string_of_int b))
 else
-	print_endline ((string_of_int a) ^ " " ^ (string_of_int (b - 1)))  
+	print_endline ((string_of_int a) ^ " " ^ (string_of_int (b - 1)))
